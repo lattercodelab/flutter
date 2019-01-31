36633c3e2551caac7bcac3528ac40880883fb1a1
Vincent Li <vincent@workspace>
2019-01-31 18:43:15 +0800

    test git.

--- flutter_lab/lib/ui/animation/scale_transition_screen.dart
+++ flutter_lab/lib/ui/animation/scale_transition_screen.dart
@@ -51,6 +51,7 @@ class _ScaleTransitionScreenState extends State<ScaleTransitionScreen> with Sing
               setState(() {
                 _scaleTrans = !_scaleTrans;
               });
+              
 
               _scaleTrans ? _controller.forward() : _controller.reverse();
             }, name: _scaleTrans ? "Zoom In" : "Zoom Out")
